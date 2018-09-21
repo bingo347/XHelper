718006824989b2d9652c73352bfff3796e7f756a
Dmitriy Belyaev <mail@d-belyaev.ru>
2018-09-21 18:18:56 +0300

    add WSLRunner

--- XHelper/App.cs
+++ XHelper/App.cs
@@ -1,12 +1,14 @@
 ﻿using System.Windows.Forms;
 using XHelper.XServer;
+using XHelper.KeyboardLayout;
+using XHelper.WSL;
 
 namespace XHelper {
     static class App {
         static void Main(string[] args) {
             using(XServerManager serverManager = new XServerManager()) {
                 if(serverManager.Success) {
-                    
+                    new WSLRunner(new KeyboardLayoutWatcher()).Run();
                 } else {
                     MessageBox.Show("Не удалось запустить XServer", "Ошибка запуска", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
---
+++ XHelper/WSL/WSLRunner.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Diagnostics;
+using XHelper.KeyboardLayout;
+
+namespace XHelper.WSL {
+    class WSLRunner {
+        private readonly Process wslProcess;
+        private readonly Mutex stdinMutex;
+        private readonly KeyboardLayoutWatcher keyboardLayoutWatcher;
+
+        public WSLRunner(KeyboardLayoutWatcher keyboardLayoutWatcher) {
+            wslProcess = Process.Start("wsl", "XHelper");
+            stdinMutex = new Mutex();
+            this.keyboardLayoutWatcher = keyboardLayoutWatcher;
+        }
+
+        public void Run() {
+            keyboardLayoutWatcher.KeyboardLayoutChanged += OnKeyboardLayoutChanged;
+        }
+
+        private void OnKeyboardLayoutChanged(string layout) {
+            SendCommand(layout);
+        }
+
+        private void SendCommand(string cmd) {
+            stdinMutex.WaitOne();
+            wslProcess.StandardInput.Write(cmd + "\n");
+            stdinMutex.ReleaseMutex();
+        }
+    }
+}
