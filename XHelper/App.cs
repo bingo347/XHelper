d06d349b505b77090f5c37260b0a7cc6269291b5
Dmitriy Belyaev <mail@d-belyaev.ru>
2018-09-21 03:42:47 +0300

    add XServerManager

--- XHelper/App.cs
+++ XHelper/App.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Windows.Forms;
+using XHelper.XServer;
 
 namespace XHelper {
     static class App {
         static void Main(string[] args) {
-            
+            using(XServerManager serverManager = new XServerManager()) {
+                if(!serverManager.Success) {
+                    MessageBox.Show("Не удалось запустить XServer", "Ошибка запуска", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                MessageBox.Show("XServer Started");
+            }
         }
     }
 }
---
+++ XHelper/XServer/XServerManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using XHelper.Properties;
+
+namespace XHelper.XServer {
+    class XServerManager : IDisposable {
+        private readonly Process XServerProcess;
+        private readonly string ConfigFile;
+
+        public XServerManager() {
+            Process[] startedXServers = Process.GetProcessesByName("vcxsrv");
+            if(startedXServers.Length > 0) {
+                XServerProcess = startedXServers[0];
+                return;
+            }
+            ConfigFile = Environment.CurrentDirectory + @"\config.xlaunch";
+            if(WriteConfigFile()) {
+                XServerProcess = Process.Start(ConfigFile);
+                return;
+            }
+            XServerProcess = null;
+        }
+
+        public bool Success {
+            get {
+                return XServerProcess != null;
+            }
+        }
+
+        public void Dispose() {
+            KillServer();
+            GC.SuppressFinalize(this);
+        }
+
+        private bool WriteConfigFile() {
+            try {
+                byte[] data = Resources.config;
+                using(FileStream fs = File.Create(ConfigFile, data.Length, FileOptions.None)) {
+                    using(BinaryWriter bw = new BinaryWriter(fs)) {
+                        bw.Write(data);
+                        bw.Close();
+                    }
+                    fs.Close();
+                }
+                return true;
+            } catch(Exception e) {
+                MessageBox.Show(e.Message, "Ошибка записи файла конфигурации", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void KillServer() {
+            if(XServerProcess == null) { return; }
+            XServerProcess.Kill();
+        }
+
+        ~XServerManager() {
+            KillServer();
+        }
+    }
+}
